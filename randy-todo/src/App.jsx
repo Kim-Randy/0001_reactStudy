1f34a0b9918c98838800c1ee7f8c21456bb1e8b6
Kim-Randy <y0god2@gmail.com>
2025-12-11 14:27:38 +0900

    첫번째 git연결 commit 연결

--- randy-todo/src/App.jsx
+++ randy-todo/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>첫 React 프로젝트!</h1>
+        <h1>첫 React 프로젝트!(git연결)</h1>
         <button onClick={()=> setCount(count+1) }>
           클릭하면 증가 : {count}
         </button>
